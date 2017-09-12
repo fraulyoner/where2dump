44281512eb74dc5fb748dee513499592be5e6454
fraulyoner <aljona.buchloh@gmail.com>
2017-09-12 23:13:39 +0200

    Initial commit: create basic map using Leaflet

---
+++ map.js
@@ -0,0 +1,26 @@
+// set up the map
+var map = new L.Map('map');
+
+// create the tile layer with correct attribution
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 20, attribution: osmAttrib});
+
+// start the map in Karlsruhe, Germany
+map.setView(new L.LatLng(49.00, 8.40), 10);
+map.addLayer(osm);
+
+  // add marker to Karlsruhe Schloss
+var marker = L.marker([49.013397, 8.404370]).addTo(map);
+marker.bindPopup("<b>Karlsruhe</b><br>Schloss Karlsruhe");
+
+// show something when clicking on map
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup.setLatLng(e.latlng)
+       .setContent("You clicked the map at " + e.latlng.toString())
+       .openOn(map);
+}
+
+map.on('click', onMapClick);
